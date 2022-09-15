da0c913db31745fd578de00bb36d7c4ae0908a2a
Favvie <favourprecious3@gmail.com>
2022-09-15 12:06:12 +0100

    remove orm details

--- models/place.py
+++ models/place.py
@@ -1,35 +1,18 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from curses import echo
 from models.base_model import BaseModel
-from sqlalchemy import create_engine, Column, Integer, String, Float, ARRAY
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-
-engine = create_engine('mysql+mysqldb://root:Christlike1_@localhost:3306\
-                       /airbnb',
-                       echo=False)
-
-Session = sessionmaker(bind=engine)
-session = Session()
-Base = declarative_base()
 
 
-class Place(BaseModel, Base):
+class Place(BaseModel):
     """ A place to stay """
-    __tablename__ = 'place'
-
-    city_id = Column(String(256))
-    user_id = Column(String(256))
-    name = Column(String(256))
-    description = Column(String(256))
-    number_rooms = Column(Integer)
-    number_bathrooms = Column(Integer)
-    max_guest = Column(Integer)
-    price_by_night = Column(Integer)
-    latitude = Column(Float)
-    longitude = Column(Float)
-    amenity_ids = Column(ARRAY)
-
-
-Base.metadata.create_all(engine)
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
--- models/state.py
+++ models/state.py
@@ -1,23 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
-from mysqlalchemy import create_engine, Column, String
-from sqlalchemy.orm import sessionmaker
-from mysqlalchemy.ext.declarative import declarative_base
 
 
-engine = create_engine('mysql+mysqldb://root:Christlike1_@localhost:3306\
-                       /airbnb',
-                       echo=False)
-
-Session = sessionmaker(bind=engine)
-session = Session()
-Base = declarative_base()
-
-
-class State(BaseModel, Base):
+class State(BaseModel):
     """ State class """
-    name = Column(String(256))
-
-
-Base.metadata.create_all(engine)
+    name = ""
