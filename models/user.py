144acca06454900dc7c15474610790e0d771cbba
Favvie <favourprecious3@gmail.com>
2022-09-16 09:51:30 +0100

    update the user class to allow connection to db

--- models/user.py
+++ models/user.py
@@ -1,11 +1,25 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
+"""This module defines a class user"""
+from curses import echo
 from models.base_model import BaseModel
+from sqlalchemy import create_engine, Column, String
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+engine = create_engine('mysql+mysqldb://root:Christlike1_@localhost:3306/hbnb_dev_db', echo=False)
+Session = sessionmaker(bind=engine)
+session = Session()
+Base = declarative_base()
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    __tablename__ = 'users'
+
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+
+
+Base.metadata.create_all(engine)
