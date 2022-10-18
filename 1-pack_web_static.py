86853922d4bdc8cd8f39ee411091ac209a6497a6
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-10-18 15:20:54 +0000

    Using Fabric

---
+++ 1-pack_web_static.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" A fabric script that generates a .tgz
+    archive
+"""
+from fabric.api import local
+import time
+
+
+def do_pack():
+    """ Archive the web_static folder using the
+        tar command
+    """
+    try:
+        local("mkdir versions")
+
+        local("tar -cvzf versions/web_static_{}."
+              "tgz web_static".format(time.strftime("%Y%m%d%H%M%S")))
+        return ("versions/web_static_{}.tgz".format(
+                time.strftime("%Y%m%d%H%M%S")))
+    except Exception as e:
+        return None
