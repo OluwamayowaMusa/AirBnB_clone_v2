8b4c56db456e3fbf4b7b40f778d1cd06238f8f1f
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-10-18 14:35:42 +0000

    Fixed Error

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Sets up web servers for the deployment
 sudo apt-get update
-sudo apt-get install nginx
+sudo apt-get -y install nginx
 sudo mkdir -p /data/
 sudo mkdir -p /data/web_static/
 sudo mkdir -p /data/web_static/releases
@@ -20,5 +20,5 @@ sudo cat << EOF | sudo tee /data/web_static/releases/test/index.html
 EOF
 sudo ln -s -f /data/web_static/releases/test /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data
-sudo sed -i "39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n" /etc/nginx/sites-enabled/default
+sudo sed -i "39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n" /etc/nginx/sites-available/default
 sudo service nginx restart
