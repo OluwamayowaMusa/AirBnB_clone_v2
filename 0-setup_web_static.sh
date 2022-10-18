ca0411d0108d276b45af2b5c0af7f83f3472aad6
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-10-18 13:10:24 +0000

    Fixed Error

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,7 +8,16 @@ sudo mkdir -p /data/web_static/releases
 sudo mkdir -p /data/web_static/shared
 sudo mkdir -p /data/web_static/releases/test/
 sudo touch /data/web_static/releases/test/index.html
-echo "Hello World" | sudo tee /data/web_static/releases/test/index.html
+sudo cat << EOF | sudo tee /data/web_static/releases/test/index.html
+<!DOCTYPE html>
+<hmtl>
+  <head>
+  </head>
+  <body>
+    <p>Hello World</p>
+  </body>
+</html>
+EOF
 sudo ln -s -f /data/web_static/releases/test /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data
 sudo sed -i "39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n" /etc/nginx/sites-enabled/default
