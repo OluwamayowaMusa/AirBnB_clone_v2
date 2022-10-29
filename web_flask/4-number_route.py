dce38f2d13e8fea89c251ea3fca442ba126c887a
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-10-29 04:28:58 +0000

    Test

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Starts a Flask Web application comprising
-    of five view functions
+    of three view functions
 """
 from flask import Flask
 
@@ -31,34 +31,6 @@ def home_c(text: str) -> str:
     """
     text = text.replace('_', ' ')
     return "C " + text
-
-
-@app.route('/python/', defaults={'text': "is cool"})
-@app.route('/python/<text>', strict_slashes=False)
-def home_python(text: str) -> str:
-    """ Displays Python followed by text
-
-    Args:
-        text(str): Text to append
-
-    Returns:
-        Python + <text>
-    """
-    text = text.replace('_', ' ')
-    return "Python " + text
-
-
-@app.route('/number/<int:n>', strict_slashes=False)
-def number(n: int) -> str:
-    """ Displays n is number
-
-    Args:
-        n(int): Number to diplay
-
-    Returns:
-        n is number
-    """
-    return f"{n} is number"
 
 
 if __name__ == "__main__":
