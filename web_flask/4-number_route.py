07d2f10266038da1fb47263372a981ee3012b3f0
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2022-10-29 04:24:14 +0000

    Dynamic routing with specified type

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -48,5 +48,11 @@ def home_python(text: str) -> str:
     return "Python " + text
 
 
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n: int) -> str:
+    """ Displays n is number """
+    return f"{n} is number"
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
