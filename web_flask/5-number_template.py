3fcbd89142b1bd7993b0a35cd85224e0f97410a2
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2024-05-22 23:40:03 +0100

    Updated files

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -61,6 +61,17 @@ def home_number(n: int) -> str:
     return "{} is a number".format(n)
 
 
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def home_number_template(n: int) -> str:
+    """ Displays n is a number
+
+    Args:
+        n(int): Number to display
+
+    Returns:
+        n is number
+    """
+    return render_template("5-number.html", n)
 
 
 if __name__ == "__main__":
