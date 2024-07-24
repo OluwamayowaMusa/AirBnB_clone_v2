9e50373185fcd4924b7ee099cec2628f6348ea31
OluwamayowaMusa <oluwamayowamusa01@gmail.com>
2024-07-24 11:04:22 +0100

    Added user

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -8,6 +8,7 @@ from fabric.api import env, local, put, run
 from datetime import datetime
 from os.path import exists, isdir
 env.hosts = ["3.84.237.196", "3.80.19.23"]
+env.user = "ubuntu"
 
 
 def do_pack():
